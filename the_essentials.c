73f92ec9ebb209d0d0f0c1d4d6d954e847f3201b
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2023-12-15 03:00:08 +0200

    Update the_essentials.c

--- the_essentials.c
+++ the_essentials.c
@@ -1,109 +1,171 @@
 #include "main.h"
 
 /**
- * _free - free pointer and set it to NULL
- * @ptr: pointer to free (void **)
+ * getc_command - get command from user input (char *)
+ * @str: user input (char *)
+ * @c_command: pointer to command to be returned (char **)
+ * @cmd_size: pointer to size of command to be returned (int *)
  * Return: void
  * Authors: Ahmed Raafat & Ahmed Abdelanasser
  */
-void _free(void **ptr)
+void getc_command(char *str, char **c_command, int *cmd_size)
 {
-	if (*ptr)
-	{
-		free(*ptr);
-		*ptr = NULL;
-	}
-}
+	int i = 0, j = 0;
 
+	while (str[i] == 2 || str[i] == ' ')
+	{
+		i++;
+	}
+	while (str[i])
+	{
+		if (str[i] == '\n')
+		{
+			if (str[i + 1] != '\n')
+			{
+				str[i] = 2;
+				break;
+			}
+			else
+			{
+				str[i] = 2;
+				i++;
+			}
+		}
+		else if (str[i] == ' ' && !last_space(str + i))
+			break;
+
+		else if (str[i] == ' ' && str[i + 1] == ' ')
+		{
+			i++;
+			continue;
+		}
+		else
+			getc_command_helper(str, c_command, cmd_size, &i, &j);
+	}
+	(*c_command)[j] = '\0';
+	alias_replace(c_command, cmd_size);
+	edit_command(c_command, cmd_size);
+}
 /**
- * _malloc - allocate memory and check if it failed
- * @size: size of memory to allocate (unsigned int)
- * Return: pointer to allocated memory (void *)
+ * last_space - check if last char in string is space
+ * @str: string to check (char *)
+ * Return: (1) if last char in string is space, (0) others (int)
  * Authors: Ahmed Raafat & Ahmed Abdelanasser
  */
-void *_malloc(unsigned int size)
+int last_space(char *str)
 {
-	void *str = malloc(size);
+	int i = 0;
 
-	if (!str)
+	while (str[i])
 	{
-		_Free(str);
-		print(STDOUT_FILENO, "./hsh :malloc failed\n", NULL);
-		exit(-1);
+		if (str[i++] != ' ')
+			return (1);
 	}
-	intail_NULL(str, size);
-	return (str);
+	return (0);
 }
-
 /**
- * _realloc - reallocate memory and check if it failed
- * @ptr: pointer to reallocate (void **)
- * @new_size: new size of memory to allocate (unsigned int)
- * Return: pointer to reallocated memory (void *)
- * Authors: Ahmed Raafat & Ahmed Abdelanasser
- */
-void *_realloc(void *ptr, int new_size)
-{
-	void *temp;
-
-	if (new_size < _strlen(ptr))
-	{
-		_Free(ptr);
-		return (NULL);
-	}
-	temp = _malloc(new_size);
-	_memcopy(temp, ptr, _strlen(ptr));
-	_Free(ptr);
-	return (temp);
-}
-
-/**
- * _read - read from file descriptor and check if it failed
- * @fd: file descriptor to read from (int)
- * @str: buffer to read into (char *)
- * @str_size: size of buffer to read into (int)
- * Return: number of characters read (int)
- * Authors: Ahmed Raafat & Ahmed Abdelanasser
- */
-int _read(int fd, char *str, int str_size)
-{
-	int read_chars = 0;
-
-	read_chars = read(fd, str, str_size);
-	if ((str)[read_chars - 1] == '\n')
-		(str)[read_chars - 1] = '\0';
-	if (read_chars == -1)
-	{
-		_Free(str);
-		exit(2);
-	}
-	return (read_chars);
-}
-
-/**
- * print - print to file descriptor
- * @fd: file descriptor to print to (int)
+ * getc_command_helper - helper function for getc_command
+ * @str: user input (char *)
+ * @c_command: pointer to command to be returned (char **)
+ * @cmd_size: pointer to size of command to be returned (int *)
+ * @i: pointer to index of user input (int *)
+ * @j: pointer to index of command to be returned (int *)
  * Return: void
  * Authors: Ahmed Raafat & Ahmed Abdelanasser
  */
-void print(int fd, ...)
+void getc_command_helper(char *str, char **c_command,
+						 int *cmd_size, int *i, int *j)
 {
-	va_list args;
-	char *c_arg, *str = NULL;
-	int write_to = 0;
-
-	va_start(args, fd);
-	write_to = fd;
-	c_arg = va_arg(args, char *);
-	str = _strdup(c_arg);
-	c_arg = va_arg(args, char *);
-	while (c_arg)
+	if (*j < *cmd_size - 2)
 	{
-		str = _realloc(str, _strlen(str) + _strlen(c_arg) + 1);
-		_strcat(str, c_arg);
-		c_arg = va_arg(args, char *);
+		(*c_command)[*j] = str[*i];
+		str[*i] = 2;
+		*i += 1;
+		*j += 1;
 	}
-	write(write_to, str, _strlen(str));
-	_Free(str);
-	va_end(args);
+	else
+	{
+		*c_command = _realloc(*c_command, *cmd_size * 2);
+		*cmd_size *= 2;
+		buffers(NULL, c_command, 1);
+	}
+}
+/**
+ * edit_command - edit command to replace $$, $?, and $<env>
+ * @str_ptr: pointer to command to be edited (char **)
+ * @str_size: pointer to size of command to be edited (int *)
+ * Return: void
+ * Authors: Ahmed Raafat & Ahmed Abdelanasser
+ */
+void edit_command(char **str_ptr, int *str_size)
+{
+	int i = 0;
+	int temp_size = *str_size;
+	char *temp = malloc(temp_size);
+	char *str = *str_ptr;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (_strcmp(str + i, "$$") == 0)
+		{
+			char pid[255];
+
+			nts(getpid(), pid);
+
+			str = replaceTxtInd(str_ptr, pid, i, i + 1);
+			i += _strlen(pid) - 1;
+		}
+		else if (_strcmp(str + i, "$?") == 0)
+		{
+			char last_exit_code[255];
+
+			nts(State, last_exit_code);
+
+			str = replaceTxtInd(str_ptr, last_exit_code, i, i + 1);
+			i += _strlen(last_exit_code) - 1;
+		}
+		else if (str[i] == '$' && str[i + 1] != '$' &&
+				 str[i + 1] != ' ' && str[i + 1])
+		{
+			edit_command_helper(str_ptr, &str, temp, &i);
+		}
+		else if (str[i] == '#')
+		{
+			if ((i && str[i - 1] == ' ') || !i)
+				str[i] = '\0';
+			break;
+		}
+	}
+	_Free(temp);
+}
+/**
+ * edit_command_helper - helper function for edit_command to replace $<env>
+ * @str_ptr: pointer to command to be edited (char **) (copy of str)
+ * @str: pointer to command to be edited (char *) (copy of str_ptr)
+ * @temp: pointer to temp string (char *) to be used in editing
+ * @index: pointer to index of command to be edited (int *) (copy of i)
+ * Return: void (edited command is returned through str_ptr)
+ * Authors: Ahmed Raafat & Ahmed Abdelanasser
+ */
+void edit_command_helper(char **str_ptr,
+						 char **str, char *temp, int *index)
+{
+	char *value;
+	int j = 0, i = *index;
+
+	while ((*str)[i + j] && (*str)[i + j] != ' ')
+		j++;
+	_memcopy(temp, (*str) + i + 1, j - 1);
+	temp[j - 1] = '\0';
+	value = get_env_value(temp);
+	if (value)
+	{
+		*str = replaceTxtInd(str_ptr, value, i, i + j - 1);
+		*index += _strlen(value) - 1;
+		_Free(value);
+	}
+	else
+	{
+		*str = replaceTxtInd(str_ptr, " ", i, i + j - 1);
+	}
 }
